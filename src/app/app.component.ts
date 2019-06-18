fab18c670d55345e49f874390acd0b1a8f9cec96
Fabien Brive <fabienbrive.pro@gmail.com>
2019-06-18 18:00:17 +0200

    mise en place userForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'OpenBooks';
+  constructor(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyC8faDdBNTthphIlwC3A-S_vUttjAkWfqw",
+      authDomain: "openbooks-e0f10.firebaseapp.com",
+      databaseURL: "https://openbooks-e0f10.firebaseio.com",
+      projectId: "openbooks-e0f10",
+      storageBucket: "",
+      messagingSenderId: "216037221160",
+      appId: "1:216037221160:web:278caafe39da4d9f"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { BookListComponent } from './book/book-list/book-list.component';
+import { SingleBookComponent } from './book/single-book/single-book.component';
+import { BookFormComponent } from './book/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { BooksService } from './services/books.service';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SignUpComponent },
+  { path: 'auth/signin', component: SignInComponent },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/new', component: BookFormComponent },
+  { path: 'books/views/:id', component: SingleBookComponent }
+
+];
+  
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SignInComponent,
+    BookListComponent,
+    SingleBookComponent,
+    BookFormComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuardService,
+    BooksService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
