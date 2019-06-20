afa18cfe20599647b5c301debef33ef515bd9ceb
Fabien Brive <fabienbrive.pro@gmail.com>
2019-06-20 10:46:15 +0200

    implémentation signIn and signOut coponent

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
-import { Router } from '@angular/router';
+  import { Component, OnInit } from '@angular/core';
+  import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+  import { AuthService } from 'src/app/services/auth.service';
+  import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
